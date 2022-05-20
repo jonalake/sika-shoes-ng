7e13487524d77191d1f21892602b6fe4494faf96
Jon Lake <jlake21@ford.com>
2022-05-20 09:56:24 -0600

    Add Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from "./app.component";
-
 import {AboutPageComponent} from "./about-page/about-page.component";
 import {HomePageComponent} from "./home-page/home-page.component";
 import {ProductPageComponent} from "./product-page/product-page.component";
 import {ProductsPageComponent} from "./products-page/products-page.component";
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {ProductListingComponent} from "./product-listing/product-listing.component";
+
+const routes: Routes = [{
+  path: '',
+  redirectTo: "/home",
+  pathMatch: "full"
+}, {
+  path: 'home',
+  component: HomePageComponent
+}, {
+  path: 'about',
+  component: AboutPageComponent
+}, {
+  path: 'products',
+  component: ProductsPageComponent
+}, {
+  path: 'products/:id',
+  component: ProductPageComponent
+}]
 
 @NgModule({
   declarations: [
@@ -22,7 +40,9 @@ import {ProductListingComponent} from "./product-listing/product-listing.compone
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
