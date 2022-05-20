41330e6a32177e62f3d1e19d2b4724c932b67780
Matt Crecraft <mcrecraft@gmail.com>
2022-05-20 14:01:19 -0500

    Matt commit

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,27 @@
-import {Component} from "@angular/core";
-import {Product} from "../product.model"
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../product.model"
+import { ShoeService } from "../shoe.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-product-page",
   templateUrl: "./product-page.component.html",
   styleUrls: ["./product-page.component.css"]
 })
-export class ProductPageComponent {
-  product?: Product;
-  error = false;
+export class ProductPageComponent implements OnInit {
+  product?: Product
+  products!: Product[]
+  error = false
+  id: string = ""
+
+  constructor(private shoeService: ShoeService, private route: ActivatedRoute) { }
+
   ngOnInit() {
-    // Get product here
+    this.id = this.route.snapshot.paramMap.get("id") || ""
+
+    this.shoeService.listShoes.subscribe(response => {
+      this.products = response.products;
+      this.product = this.products.find(product => product.id === +this.id)
+    })
   }
 }
